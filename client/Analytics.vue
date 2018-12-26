20e2977c50138e58f927bfcc9b53189a2599c25f
Yersultann <yersultan.u@gmail.com>
2018-12-26 16:28:45 +0600

    add analytics component

---
+++ client/Analytics.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="analytics">
+		<h2 class="analytics__title">{{ post.title }}</h2>
+		<p class="analytics__content">{{ post.content }}</p>
+	</div>
+</template>
+<script>
+	export default { 
+ 		props: ['post']
+	}
+</script>
+<style lang="scss" scoped>
+	.analytics {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		margin: 0;
+		padding: 0;
+
+		font-family: "Roboto", sans-serif;
+
+		&__title {
+			display: inline-block;
+			margin-bottom: 13px;
+
+			font-size: 30px;
+			line-height: 33px;
+
+			color: #6e6e70;
+
+			vertical-align: middle;
+		}
+
+		&__content {
+			display: inline-block;
+			margin: 0;
+
+			font-size: 13px;
+			line-height: 19px;
+			font-style: italic;
+
+			color: rgba(0, 0, 0, 0.38);
+		}
+	}
+</style>
--- client/App.vue
+++ client/App.vue
@@ -20,23 +20,35 @@
 
     <Title secondary>{{ msg }}</Title>
     <Date> {{ date }} </Date>
+    <ul>
+      <li v-for="item in analytics">
+        <analytics :post="item"></analytics>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import Title from './Title.vue';
 import Date from './Date.vue';
+import Analytics from "./Analytics.vue";
 
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      date: 'April 2016'
+      date: 'April 2016',
+      analytics: [
+        {title: "20+", content: "Hours of work"}, 
+        {title: "10 000+", content: "Views"}, 
+        {title: "12", content: "Team members"}
+      ]
     }
   },
   components: {
-    Title
+    Title,
+    analytics: Analytics
   }
 }
 </script>

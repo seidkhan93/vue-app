04a4488eed565db0b06e6d8f3fdc924af3d0891f
Nursultan <nberkimbayev@gmail.com>
2018-12-27 19:00:38 +0600

    feat: Add fix

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './client/main.js',
   output: {
-    path: path.resolve(__dirname, './build'),
-    publicPath: '/build/',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
     filename: 'build.js'
   },
   module: {

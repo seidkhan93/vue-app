3a35c6642007c8f6769c41187c8b008decb951a1
Seidkhan <seidkhan93@mail.ru>
2018-12-28 10:51:52 +0600

    add feature/reset

--- client/main.js
+++ client/main.js
@@ -1,3 +1,7 @@
+import "_reset.scss"
+import "_main.scss"
+import "../node_modules/bootstrap/scss/bootstrap-grid"
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'

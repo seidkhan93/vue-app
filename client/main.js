930068522b88b0b0dc2c47eb276f7768adc329e1
Seidkhan <seidkhan93@mail.ru>
2019-01-06 21:57:30 +0600

    some fix

--- client/main.js
+++ client/main.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-import "./_reset.scss"
-import "./_main.scss"
-import "bootstrap/scss/bootstrap-grid.scss"
-=======
 import "./_reset.scss";
 import "./_main.scss";
 import "bootstrap/scss/bootstrap-grid.scss";
->>>>>>> 02e13fd5c75eb2b7151c595f8ec2808e91a0e192
+
 
 import Vue from 'vue';
 import VueRouter from 'vue-router';
